a45270f778e491dbcc03b912fcc55f7846302776
Michael Friedman <frimicc@gmail.com>
2017-04-04 18:38:40 -0800

    Added eslint and no-semicolon rules.

--- src/dog.js
+++ src/dog.js
@@ -1,11 +1,11 @@
 class Dog {
-    constructor(name) {
-        this.name = name
-    }
+  constructor(name) {
+    this.name = name
+  }
 
-    bark() {
-        return `Woof woof, I am ${this.name}`
-    }
+  bark() {
+    return `Woof woof, I am ${this.name}`
+  }
 }
 
 export default Dog
