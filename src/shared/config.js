4d5743d1585ed38fb4f137316ca06e4de3943d58
Michael Friedman <frimicc@gmail.com>
2017-04-09 12:48:10 -0800

    Added webpack with babel extensions. Now runs via wds.

--- src/shared/config.js
+++ src/shared/config.js
@@ -4,3 +4,9 @@ export const WEB_PORT = process.env.PORT || 37433
 export const STATIC_PATH = '/static'
 export const APP_NAME = 'Hello World'
 
+export const WDS_PORT = 37434
+export const WDS_HOST = 'friedman.polyvore.net'
+
+export const APP_CONTAINER_CLASS = 'js-app'
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
+
