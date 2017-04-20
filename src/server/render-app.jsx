f696a64dfee772efeb4063dfd619f07c33b98710
Michael Friedman <frimicc@gmail.com>
2017-04-19 19:43:10 -0800

    Added dynamic titles with react-helmet.

--- src/server/render-app.jsx
+++ src/server/render-app.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import Helmet from 'react-helmet'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router'
 
@@ -19,12 +20,14 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
       </StaticRouter>
     </Provider>
   )
+  const head = Helmet.rewind()
 
   return (
     `<!doctype html>
     <html>
     <head>
-        <title>FIXME</title>
+        ${head.title}
+        ${head.meta}
         <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
     </head>
     <body>
